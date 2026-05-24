6c1260bef5e1bf1cb5aec9f7bf2ac759e2f687f2
sravanthi31qa-star <sravanthi31.qa@gmail.com>
2026-05-24 12:27:02 +0530

    my second commit

--- tests/sample2.spec.ts
+++ tests/sample2.spec.ts
@@ -62,8 +62,8 @@ test.describe('SauceDemo Playwright Automation', () => {
     await page.click('.shopping_cart_link');
     await page.click('[data-test="checkout"]');
 
-    await page.fill('[data-test="firstName"]', 'John');
-    await page.fill('[data-test="lastName"]', 'Doe');
+    await page.fill('[data-test="firstName"]', 'sravanthi');
+    await page.fill('[data-test="lastName"]', 'isank');
     await page.fill('[data-test="postalCode"]', '12345');
     await page.click('[data-test="continue"]');
 
