f611f9acf4819ba920a5f4a6fa82f1500636f5dc
Sravanthi <isanakulas@gmail.com>
2026-05-29 21:46:38 +0530

    Replaced old code with Ejada SampleTest project

--- utils/dataGenerator.ts
+++
@@ -1,9 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-export function generateRandomCheckoutDetails() {
-  return {
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
-    postalCode: faker.location.zipCode('#####'),
-  };
-}
